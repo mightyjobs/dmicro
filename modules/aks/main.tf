dabaf169eba0deba6edc1790aab5ef5e5f6a37ea
mightyjobs <jobs@oyetoro.com>
2024-10-03 19:51:09 +0200

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,1 +1,11 @@
+resource "azurerm_resource_group" "aks" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
+resource "azurerm_virtual_network" "aks" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+}
