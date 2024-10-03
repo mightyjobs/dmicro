a2972ccaea7c77a58149d82b4d82f01adbe27fdf
mightyjobs <jobs@oyetoro.com>
2024-10-03 19:59:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,3 +13,16 @@ module "aks" {
     Environment = "Development"
   }
 }
+
+output "cluster_name" {
+  value = module.aks.cluster_name
+}
+
+output "resource_group_name" {
+  value = module.aks.resource_group_name
+}
+
+output "kube_config" {
+  value     = module.aks.kube_config
+  sensitive = true
+}
