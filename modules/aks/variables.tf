45f0c91e5f344fd5cd8cfa492ba2b394b874d5af
mightyjobs <jobs@oyetoro.com>
2024-10-03 19:53:44 +0200

    Update variables.tf

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -9,3 +9,19 @@ variable "location" {
 variable "resource_group_name" {
   description = "Name of the resource group to create"
 }
+
+variable "node_count" {
+  description = "The number of nodes in the AKS cluster"
+  default     = 1
+}
+
+variable "vm_size" {
+  description = "The size of the Virtual Machine"
+  default     = "Standard_B2s"
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
