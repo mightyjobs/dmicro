b1fc1c85f750ed7aacd5eccf69570b4a25ee9a4f
mightyjobs <jobs@oyetoro.com>
2024-10-03 19:52:11 +0200

    Update variables.tf

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -1,1 +1,11 @@
+variable "prefix" {
+  description = "A prefix used for all resources"
+}
 
+variable "location" {
+  description = "The Azure Region in which all resources should be provisioned"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group to create"
+}
