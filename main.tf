8c9bbd1d65e46746a6c37c46b90fb639634f6ba5
mightyjobs <jobs@oyetoro.com>
2024-10-03 19:58:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
 provider "azurerm" {
   features {}
 }
+
+module "aks" {
+  source              = "./modules/aks"
+  prefix              = "myaks"
+  location            = "East US"
+  resource_group_name = "my-aks-rg"
+  node_count          = 2
+  vm_size             = "Standard_B2s"
+  tags = {
+    Environment = "Development"
+  }
+}
